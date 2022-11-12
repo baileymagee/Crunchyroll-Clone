9e4d027ba125eed1f5b8933c8e529b794a8d35d1
Fahd Ahsan <sfahsan@umich.edu>
2022-11-11 19:06:24 -0500

    doneish

---
+++ script.js
@@ -0,0 +1,10 @@
+const backGround = document.querySelector("#main_img");
+
+const buttons = document.querySelectorAll(".carousel_button");
+
+for (let button of buttons) {
+    // button.addEventListener("click", () => {
+    //     if (button.classList.includes(".main_img"))
+    // })
+    console.log(Array.isArray(backGround.classList))
+}
