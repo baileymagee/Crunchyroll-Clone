3bb79a8962310b226a32d9f79ef6a2ce9f07315b
Henry W <henry.woodmansee@gmail.com>
2022-11-13 12:52:01 -0800

    mid-size media query

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const backGround = document.querySelector("#main_img");
+const backGround = document.querySelector("#slider-container");
 
 const buttons = document.querySelectorAll(".carousel_button");
+const buttonArray = Array.from(buttons)
+
+const carousel = document.querySelector('#carousel-images')
 
 for (let button of buttons) {
-    // button.addEventListener("click", () => {
-    //     if (button.classList.includes(".main_img"))
-    // })
-    console.log(Array.isArray(backGround.classList))
+    const index = buttonArray.indexOf(button)
+    const selected = `image-${index}`
+    const isEven = index % 2 === 0
+    button.addEventListener("click", () => {
+        carousel.classList.remove(carousel.classList.value)
+        carousel.classList.add(selected)
+
+        if (isEven) {
+            backGround.classList.remove('background-2')
+            backGround.classList.add('background-1')
+        } else {
+            backGround.classList.remove('background-1')
+            backGround.classList.add('background-2')
+        }
+    })
 }
