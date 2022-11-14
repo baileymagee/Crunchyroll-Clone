dbba3fcc3655c3d31b685b04aaaf1a4701e7a061
Henry W <henry.woodmansee@gmail.com>
2022-11-14 07:04:50 -0800

    fixed full size carousel

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ const backGround = document.querySelector("#slider-container");
 const buttons = document.querySelectorAll(".carousel_button");
 const buttonArray = Array.from(buttons)
 
+
+const browseButton = document.querySelector('#browse')
+const dropDown = document.querySelector('.drop-down')
+
 const carousel = document.querySelector('#carousel-images')
 
 for (let button of buttons) {
-    const index = buttonArray.indexOf(button)
+    let index = buttonArray.indexOf(button)
+    if (index > 5) {
+        index -= 6
+    }
     const selected = `image-${index}`
     const isEven = index % 2 === 0
     button.addEventListener("click", () => {
         carousel.classList.remove(carousel.classList.value)
         carousel.classList.add(selected)
-
         if (isEven) {
             backGround.classList.remove('background-2')
             backGround.classList.add('background-1')
